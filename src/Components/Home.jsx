a9454348867805b2ad8e089ce6bf9515d5051c75
Akshay Sawant <1akshaysawant@gmail.com>
2025-01-22 18:22:47 +0530

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,46 +1,44 @@
-import React, { useEffect, useRef } from 'react'
-import Img from "../../public/vite.svg";
+import React, { useEffect, useRef, useState } from 'react'
 import { IoMdClose } from "react-icons/io";
-import { useState } from 'react';
 
 function Home() {
-  const [modalOpen, setModalOpen] = useState(false)
-  const [isintial, setisintial] = useState(true)
-  let timerid = useRef(null);
+  const [ismodalopen, setismodalopen] = useState(false)
+  const [isinitial, setisinitial] = useState(true)
+  const timerid = useRef(null)
   const modalhandler = (duration) => {
     clearInterval(timerid.current)
     timerid.current = setInterval(() => {
-      setModalOpen(true)
+      setismodalopen(true)
     }, duration);
   }
   useEffect(() => {
-    if (isintial) {
+    if (isinitial) {
       modalhandler(10000)
-      setisintial(false)
+      setisinitial(false)
     }
-    return ()=> clearInterval(timerid.current)
+    return () => clearInterval(timerid.current)
   }, [])
-  const handleClose = () => {
-    setModalOpen(false)
-    modalhandler(30000)
+  const handleclose = () => {
+    setismodalopen(false)
+    modalhandler(20000)
   }
 
-
   return (
-    <div className='w-full font-[gilroy] h-screen py-32 px-5'>
-      <h2 className='md:text-5xl font-semibold text-center text-3xl'>welcome to the homepage</h2>
-      <div className='w-full flex justify-center gap-5 flex-col items-center'>
-        <button onClick={() => setModalOpen(true)} className='text-white mt-5 bg-emerald-500 rounded-full py-1 px-3 text-sm md:text-xs'>New Arrival</button>
-        {modalOpen && <div className='w-[50vh] overflow-hidden relative h-[50vh] bg-white rounded-2xl opacity-90'>
-          <div className='imgdiv h-52 w-full'>
-            <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1608571423902-eed4a5ad8108?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-            <p className='font-semibold text-xs mt-5 text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non, ad temporibus dolore sit quam placeat laboriosam aliquid beatae repudiandae dignissimos neque reprehenderit blanditiis optio voluptatem.</p>
-            <button onClick={handleClose} className='bg-black opacity-70 absolute top-2 right-2 text-white h-5 w-5 rounded-full flex justify-center items-center'><IoMdClose /></button>
-          </div>
-        </div>}
-      </div>
+    <div className='w-full h-screen py-20 bg-zinc-300 flex flex-col items-center font-[gilroy]'>
+      <h2 className='font-semibold text-center text-2xl my-5 sm:text-5xl '>Welcome to my Homepage</h2>
+      <p className='font-semibold text-center text-sm '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, voluptates quidem! Recusandae quaerat soluta nobis, enim impedit esse ratione quibusdam, doloremque, aspernatur earum hic excepturi.</p>
+      {ismodalopen && <div className='md:w-[50vh] w-[40vh] relative overflow-hidden h-[50vh] rounded-2xl bg-zinc-200 shadow-lg mt-5'>
+        <div className='w-full h-52 '>
+          <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1608571424266-edeb9bbefdec?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+        </div>
+        <p className='text-xs mt-5 font-semibold text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio culpa sequi dignissimos qui sint nobis ipsa. Officia odit itaque iste inventore incidunt autem neque ut exercitationem, quam rerum dolor quasi?</p>
+        <button onClick={handleclose} className='absolute h-5 w-5 bg-black top-2 flex justify-center items-center text-white rounded-full opacity-75 right-2'><IoMdClose /></button>
+      </div>}
     </div>
   )
+
+
 }
 
 export default Home
